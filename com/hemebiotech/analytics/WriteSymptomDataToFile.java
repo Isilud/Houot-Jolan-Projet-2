8f9cf809bfe78147b1d3d0df4e4286d2828481db
Jolan Houot <r88s3gs52f75-student@anonymous-codio.com>
2023-04-17 09:22:41 +0000

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -5,4 +5,24 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	private static String path = "result.out";
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms){
+
+        try {
+            FileWriter fileWriter = new FileWriter(path, false);
+
+                for ( Map.Entry<String, Integer> symptomData : symptoms.entrySet()){
+                    fileWriter.write(symptomData.getKey() + " : " + symptomData.getValue() + "\n");
+                }
+
+            fileWriter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
