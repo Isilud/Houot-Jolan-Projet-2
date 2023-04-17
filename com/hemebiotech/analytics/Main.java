135ffe1ae0867f4f4c13e5f432fde12129f75b4f
Jolan Houot <r88s3gs52f75-student@anonymous-codio.com>
2023-04-17 10:12:45 +0000

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -6,7 +6,11 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Main");
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile();
+        AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+        counter.writeSymptoms(counter.sortSymptoms(counter.countSymptoms(counter.getSymptoms())));
 
     }
 }
